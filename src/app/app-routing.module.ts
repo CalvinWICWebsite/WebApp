a8e36e9be9c85a447c9201fed317fe967889ec0d
Emily Costa <emilycosta@Emilys-MacBook-Pro-4.local>
2021-02-10 15:45:00 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,8 +20,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
   exports: [RouterModule],
 
 })
